779fabba7ad08c7c6c12de29a9c20162ded195ba
Tanya Hodges <tanyac52003@gmail.com>
2020-05-28 16:32:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+  modified_array = []
+  index = 0 
+  while index < array.length 
+    modified_array << yield(array[index])
+    index += 1 
+  end 
+    modified_array
+end 
 
